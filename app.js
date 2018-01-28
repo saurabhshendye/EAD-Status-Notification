0a27122000ff71a92a907e23e7d61ab899b652bc
saurabhs <saurabh.shendye@gmail.com>
2018-01-28 00:21:59 -0800

    Implemented with AWS SES

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 var rus = require('req-uscis-status')
 var nodemailer = require('nodemailer')
-
+var AWS = require('aws-sdk')
 
 var fs = require("fs"),
     json;
 
+AWS.config.loadFromPath('aws-config.json');
 json = readJsonFileSync('config.json')
+
+
+rus.getStatus(json.receiptNum, 
+    function(err, statusObject) { 
+
+        var params = {
+            Destination: { /* required */
+                ToAddresses: [
+                json.maillist[0],
+                /* more items */
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "EAD status: \n" + statusObject.statusLongText
+                }
+                },
+                Subject: {
+                Charset: 'UTF-8',
+                Data: 'EAD Status ' + statusObject.statusShortText
+                }
+                },
+            Source: json.usename
+        };
+
+        var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+        sendPromise.then(
+            function(data) {
+              console.log(data.MessageId);
+            }).catch(
+              function(err) {
+              console.error(err, err.stack);
+            });
+    } 
+);
 
 
 function readJsonFileSync(filepath, encoding){
@@ -17,26 +56,26 @@ function readJsonFileSync(filepath, encoding){
     return JSON.parse(file)
 }
 
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: json.username,
-        pass: json.password
-    }
-});
+// var transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//         user: json.username,
+//         pass: json.password
+//     }
+// });
 
-rus.getStatus(json.receiptNum, 
-  function(err, statusObject) { 
-        transporter.sendMail({
-            from: "Saurabh Shendye"+"<"+json.username+">",
-            to: json.maillist,
-            subject: "FINAL TEST: " + statusObject.statusShortText,
-            text : "EAD status: \n" + statusObject.statusLongText
-        },(err, success)=>{
-            if (err){
-                console.log(err);   
-            }
-        });
-  } 
-);
+// rus.getStatus(json.receiptNum, 
+//   function(err, statusObject) { 
+//         transporter.sendMail({
+//             from: "Saurabh Shendye"+"<"+json.username+">",
+//             to: json.maillist,
+//             subject: "FINAL TEST: " + statusObject.statusShortText,
+//             text : "EAD status: \n" + statusObject.statusLongText
+//         },(err, success)=>{
+//             if (err){
+//                 console.log(err);   
+//             }
+//         });
+//   } 
+// );
 
